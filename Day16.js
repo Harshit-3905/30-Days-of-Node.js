15cbba230ae381e8f42e2ec435fb26d16631fc17
Harshit-3905 <harshitjoshileet@gmail.com>
2024-02-16 22:25:40 +0530

    Add Day16

---
+++ Day16.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://temp:temp@cluster0.pmenxcl.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", function () {
+      console.log("Connected to DB");
+    });
+  } catch (error) {
+    console.error("Failed to connect to DB", error);
+  }
+}
+
+connectToMongoDB();
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
